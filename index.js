2f3002e02c7a4b4cd7766f4c57a303ad64ef59e5
NamanArora <naman.arora11@gmail.com>
2017-12-30 14:45:39 +0530

    Clear everything for scratchview

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+import React, { Component } from 'react';
+import {
+  Text,
 
-import React, { PropTypes, Component } from 'react';
+} from 'react-native';
+import PropTypes from 'prop-types';
+
 import { 
   requireNativeComponent, 
   View,
@@ -11,90 +16,23 @@ import {
 class SketchView extends Component {
   constructor(props) {
     super(props);
-    this.onChange = this.onChange.bind(this);
-    this.subscriptions = [];
-  }
-
-  onChange(event) {
-    console.log('save event: ',event.nativeEvent);
-    if (event.nativeEvent.type === "onSaveSketch") {
-
-      if (!this.props.onSaveSketch) {
-        return;
-      }
-
-      this.props.onSaveSketch({
-        localFilePath: event.nativeEvent.event.localFilePath,
-        imageWidth: event.nativeEvent.event.imageWidth,
-        imageHeight: event.nativeEvent.event.imageHeight
-      });
-    }
-  }
-
-  componentDidMount() {
-    if (this.props.onSaveSketch) {
-      let sub = DeviceEventEmitter.addListener(
-        'onSaveSketch',
-        this.props.onSaveSketch
-      );
-      this.subscriptions.push(sub);
-    }
-  }
-
-  componentWillUnmount() {
-    this.subscriptions.forEach(sub => sub.remove());
-    this.subscriptions = [];
   }
 
   render() {
     return (
-      <RNSketchView {... this.props} onChange={this.onChange}/>
-    );
-  }
-
-  clearSketch() {
-    UIManager.dispatchViewManagerCommand(
-      findNodeHandle(this),
-      UIManager.RNSketchView.Commands.clearSketch,
-      [],
-    );
-  }
-
-  saveSketch() {
-    UIManager.dispatchViewManagerCommand(
-      findNodeHandle(this),
-      UIManager.RNSketchView.Commands.saveSketch,
-      [],
-    );
-  }
-
-  changeTool(toolId) {
-    UIManager.dispatchViewManagerCommand(
-      findNodeHandle(this),
-      UIManager.RNSketchView.Commands.changeTool,
-      [toolId],
+      <View >
+      <RNSketchView style={this.props.sketchStyle} {... this.props} onChange={this.onChange}/>
+      <Text style={this.props.textStyle} {... this.textProps}> {this.props.text} </Text>
+      </View>
     );
   }
 
 }
-
-SketchView.constants = {
-  toolType: {
-    pen: {
-      id: 0,
-      name: 'Pen',
-    },
-    eraser: {
-      id: 1,
-      name: 'Eraser'
-    }
-  }
-};
-
 SketchView.propTypes = {
   ...View.propTypes, // include the default view properties
   selectedTool: PropTypes.number,
-  localSourceImagePath: PropTypes.string
+  localSourceImagePath: PropTypes.string,
+  text : PropTypes.string
 };
 
 let RNSketchView = requireNativeComponent('RNSketchView', SketchView, {
